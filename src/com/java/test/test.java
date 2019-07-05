cc289e23e327d3fac0810bd55a7804a5a1fcd808
zl666-999 <1024132271@qq.com>
2019-07-05 11:10:57 +0800

    Initial commit

---
+++ src/com/java/test/test.java
@@ -0,0 +1,12 @@
+package com.java.test;
+
+/**
+ * @author zl
+ * @create 2019-07-05 10:33
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("添加到github");
+    }
+
+}
